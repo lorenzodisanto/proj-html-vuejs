fb54f95cb110c3abc6189cff2f81da1a5de377a2
lorenzodisanto <lore.disanto95@gmail.com>
2024-02-19 20:00:15 +0100

    feat: add links and contents

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,11 +1,96 @@
-<script></script>
+<script>
+export default {
+  props: {
+    links: Array,
+  },
+};
+</script>
 
 <template>
-  <h2>App Header</h2>
+  <header>
+    <div class="container-header">
+      <div>
+        <img src="../assets/img/logo-autocar11.png" alt="" />
+      </div>
+      <div>
+        <ul>
+          <li v-for="link in links">
+            <a :href="link.url">{{ link.text }}</a>
+          </li>
+        </ul>
+      </div>
+      <div class="btn-header">
+        <img src="../assets/padlock.png" alt="" />
+        <span>My Account</span>
+      </div>
+      <div class="btn-header">
+        <img src="../assets/add.png" alt="" />
+        <span>Add Car</span>
+      </div>
+      <div class="menu">
+        <font-awesome-icon icon="fa-solid fa-bars" />
+      </div>
+      <div class="btn-header">
+        <img src="../assets/headphones.png" alt="" />
+        <span>+1(234)567 89 10</span>
+      </div>
+    </div>
+  </header>
 </template>
 
 <style lang="scss" scoped>
-h2 {
-  color: green;
+@use "../styles/partials/variables" as *;
+header {
+  background-color: $white;
+  color: $black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .container-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 1400px;
+
+    img {
+      width: 200px;
+      padding-top: 5px;
+    }
+
+    li {
+      display: inline-block;
+      padding: 10px;
+
+      a {
+        color: $black;
+      }
+    }
+
+    .btn-header {
+      background-color: $black;
+      height: 70px;
+      color: $white;
+      border-radius: 5px;
+      padding: 10px;
+      font-size: 1.2rem;
+      font-weight: 500;
+      cursor: pointer;
+
+      img {
+        width: auto;
+        height: 90%;
+        filter: invert(100%);
+        padding-right: 5px;
+      }
+    }
+
+    .menu {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-size: 2rem;
+    }
+  }
 }
 </style>
