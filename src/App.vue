23ccf8985263e1116c4ee8ce17000677132f9a4f
lorenzodisanto <lore.disanto95@gmail.com>
2024-02-19 19:59:07 +0100

    feat: add header and footer props

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,96 @@ export default {
   data() {
     return {
       store,
+      headerLinks: [
+        {
+          text: "Home",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "About Us",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "Vehicles",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "Dealers",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "Packages",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "Blog",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "Contact",
+          url: "#",
+          current: false,
+        },
+      ],
+
+      footerMenus: [
+        {
+          title: "My Account",
+          links: [
+            {
+              url: "#",
+              text: "My Account",
+            },
+            {
+              url: "#",
+              text: "Checkout",
+            },
+            {
+              url: "#",
+              text: "Cart",
+            },
+            {
+              url: "#",
+              text: "Packages",
+            },
+            {
+              url: "#",
+              text: "Add Car",
+            },
+          ],
+        },
+        {
+          title: "Quick Links",
+          links: [
+            {
+              url: "#",
+              text: "Home",
+            },
+            {
+              url: "#",
+              text: "About Us",
+            },
+            {
+              url: "#",
+              text: "Blog",
+            },
+            {
+              url: "#",
+              text: "Contact",
+            },
+            {
+              url: "#",
+              text: "Refund & Returns",
+            },
+          ],
+        },
+      ],
     };
   },
 
@@ -16,10 +106,9 @@ export default {
 </script>
 
 <template>
-  <h1>{{ store.title }}</h1>
-  <app-header></app-header>
+  <app-header :links="headerLinks"></app-header>
   <app-main> </app-main>
-  <app-footer></app-footer>
+  <app-footer :menus="footerMenus"></app-footer>
 </template>
 
 <style lang="scss">
