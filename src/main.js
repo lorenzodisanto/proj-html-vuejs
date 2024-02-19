aaf7b8720fe5e7be3e238a487e9dc671b27889f7
lorenzodisanto <lore.disanto95@gmail.com>
2024-02-19 19:58:36 +0100

    feat: add bootstrap and fontawesome

--- src/main.js
+++ src/main.js
@@ -1,4 +1,26 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+// Import all of Bootstrap's JS
+import * as bootstrap from "bootstrap";
+
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* import specific icons */
+import {
+  faInstagram,
+  faFacebookF,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
+/* add icons to the library */
+library.add(faInstagram, faFacebookF, faTwitter, faBars);
+
+const app = createApp(App);
+app.component("font-awesome-icon", FontAwesomeIcon);
+app.mount("#app");
