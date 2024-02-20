b24e9949e7b6f4c3f92cd83cd13052d07862e178
lorenzodisanto <lore.disanto95@gmail.com>
2024-02-20 23:22:45 +0100

    feat: import components

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,5 +1,8 @@
 <script>
 import AppHero from "./sections/AppHero.vue";
+import AppCars from "./sections/AppCars.vue";
+import AppKnowUs from "./sections/AppKnowUs.vue";
+
 import AppOpinions from "./sections/AppOpinions.vue";
 import AppArticles from "./sections/AppArticles.vue";
 
@@ -7,20 +10,16 @@ export default {
   data() {
     return {};
   },
-  components: { AppHero, AppOpinions, AppArticles },
+  components: { AppHero, AppCars, AppKnowUs, AppOpinions, AppArticles },
 };
 </script>
 
 <template>
   <app-hero></app-hero>
+  <app-cars></app-cars>
+  <app-know-us></app-know-us>
   <app-opinions></app-opinions>
   <app-articles></app-articles>
 </template>
 
-<style lang="scss" scoped>
-main {
-  border: 1px dashed red;
-  padding: 200px;
-  text-align: center;
-}
-</style>
+<style lang="scss" scoped></style>
